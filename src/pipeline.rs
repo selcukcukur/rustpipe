34ae6724d092d9f96c89ea1c1dc7d7ff2da84333
selcukcukur <selcukcukur@outlook.com.tr>
2026-06-09 22:57:21 +0300

    feat(pipeline): add pipeline struct with step chaining and execution

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -1,3 +1,25 @@
 pub trait Pipe<T> {
     fn handle(&self, input: T) -> T;
 }
+
+pub struct Pipeline<T> {
+    steps: Vec<Box<dyn Pipe<T>>>,
+}
+
+impl<T> Pipeline<T> {
+    pub fn new() -> Self {
+        Self { steps: Vec::new() }
+    }
+
+    pub fn add<P: Pipe<T> + 'static>(mut self, step: P) -> Self {
+        self.steps.push(Box::new(step));
+        self
+    }
+
+    pub fn execute(&self, mut input: T) -> T {
+        for step in &self.steps {
+            input = step.handle(input);
+        }
+        input
+    }
+}
